aaa2d7fc3860ef9068d71ad001dceeca51ee6d82
eternae2026 <rnattinha@gmail.com>
2026-05-26 19:41:55 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
-
+export default function Home() {
+  return (
+    <div style={{
+      fontFamily: 'Arial',
+      padding: '40px'
+    }}>
+      <h1>Eternae ERP</h1>
+      <p>Sistema em desenvolvimento 🚀</p>
+    </div>
+  )
+}
